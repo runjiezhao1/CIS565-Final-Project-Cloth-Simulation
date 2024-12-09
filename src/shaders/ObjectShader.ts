a0f1e92a8e03ed697abd30e3b853dbc35e85f2e6
Runjie Zhao <runjiezhao8@gmail.com>
2024-12-08 22:05:24 -0500

    add func to obj shader

--- src/shaders/ObjectShader.ts
+++ src/shaders/ObjectShader.ts
@@ -92,4 +92,41 @@ export class ObjectShader {
     getMaterialShader() {
         return this.materialShader;
     }
+
+    textureShader = `
+        struct TransformData {
+        model: mat4x4<f32>,
+        view: mat4x4<f32>,
+        projection: mat4x4<f32>,
+        };
+        @group(0) @binding(0) var<uniform> transformUBO: TransformData;
+        @group(0) @binding(1) var myTexture: texture_2d<f32>;
+        @group(0) @binding(2) var mySampler: sampler;
+
+
+        struct Fragment {
+            @builtin(position) Position : vec4<f32>,
+            @location(0) Color : vec4<f32>,
+            @location(1) TexCoord : vec2<f32>,
+        };
+
+        @vertex
+        fn vs_main(@location(0) vertexPosition: vec3<f32>, @location(1) vertexTexCoord: vec2<f32>) -> Fragment {
+            var output : Fragment;
+            output.Position = transformUBO.projection * transformUBO.view * transformUBO.model * vec4<f32>(vertexPosition, 1);
+            output.Color = vec4<f32>(normalize(vertexPosition), 1.0);
+            output.TexCoord = vertexTexCoord;
+            return output;
+        }
+
+        @fragment
+        fn fs_main(@location(0) Color: vec4<f32>, @location(1) TexCoord: vec2<f32>) -> @location(0) vec4<f32> {
+            let texColor: vec4<f32> = textureSample(myTexture, mySampler, TexCoord);
+            return Color;
+        }
+    `
+
+    getObjTextureShader(){
+        return this.textureShader;
+    }
 }
